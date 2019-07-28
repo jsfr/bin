fc8c8e5dc7122c01ffeede222b9e49923aece8e5
Jens Fredskov <jensfredskov@protonmail.ch>
2019-07-28 14:58:10 +0200

    YOLO: Ugh. Bad rebase.

--- spaces.sh
+++ spaces.sh
@@ -6,10 +6,31 @@ function join() {
         echo "$*"
 }
 
-space_list=$(yabai -m query --spaces | jq -r "map(select(.windows > 0)) | .[].index")
-active_space=$(yabai -m query --spaces --space | jq -r ".index")
+number_of_sticky_windows=$(
+        yabai -m query --windows |
+                jq -r ".[] | select(.sticky == 1) | .id" |
+                sort -n |
+                uniq |
+                wc -l |
+                awk '{print $1}'
+)
 
-all_spaces=$(echo "$space_list $active_space" | tr " " "\n" | sort -n | uniq)
-all_spaces=$(join , ${all_spaces[@]})
+active_space=$(
+        yabai -m query --spaces --space |
+                jq -r ".index"
+)
+
+all_spaces=$(
+        yabai -m query --spaces |
+                jq -r ".[]
+                        | select(.windows | length > ${number_of_sticky_windows})
+                        | select(.\"native-fullscreen\" != 1)
+                        | .index" |
+                xargs echo "$active_space" |
+                xargs -n1 |
+                sort -n -u |
+                xargs |
+                tr " " ","
+)
 
 hs -c "spaces.update({$all_spaces}, $active_space)"
