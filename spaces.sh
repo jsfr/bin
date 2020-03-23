0107870188d473451caaeeabecac9d2a336e93c9
Jens Fredskov <jensfredskov@protonmail.ch>
2020-03-23 18:52:28 +0100

    YOLO: Don't push this commit

--- spaces.sh
+++ spaces.sh
@@ -1,36 +1,26 @@
-#!/bin/bash
-
-function join() {
-        local IFS="$1"
-        shift
-        echo "$*"
-}
+#!/usr/bin/env sh
 
 number_of_sticky_windows=$(
         yabai -m query --windows |
-                jq -r ".[] | select(.sticky == 1) | .id" |
-                sort -n |
-                uniq |
-                wc -l |
-                awk '{print $1}'
+                jq -r "[.[] | select(.sticky == 1) | .id] | unique | length"
 )
 
-active_space=$(
-        yabai -m query --spaces --space |
-                jq -r ".index"
-)
-
-all_spaces=$(
+params=$(
         yabai -m query --spaces |
-                jq -r ".[]
-                        | select(.windows | length > ${number_of_sticky_windows})
-                        | select(.\"native-fullscreen\" != 1)
-                        | .index" |
-                xargs echo "$active_space" |
-                xargs -n1 |
-                sort -n -u |
-                xargs |
-                tr " " ","
+                jq -r "[.[] | select((.windows | length > ${number_of_sticky_windows}) or .focused == 1)]
+                        | reduce .[] as \$i (
+                                {shift: 0, focused: 0, spaces: []};
+                                if \$i.\"native-fullscreen\" == 1 then
+                                        {shift: (.shift + 1), focused: .focused, spaces: (.spaces + [\"'f'\"])}
+                                elif \$i.focused == 1 then
+                                        {shift: .shift, focused: (\$i.index - .shift | tostring), spaces: (.spaces + [\$i.index - .shift | tostring])}
+                                else
+                                        {shift: .shift, focused: .focused, spaces: (.spaces + [\$i.index - .shift | tostring])}
+                                end
+                            )
+                        | [(.spaces | join(\",\") | \"{\" + . + \"}\"), .focused]
+                        | join(\",\")
+                "
 )
 
-hs -c "spaces.update({$all_spaces}, $active_space)"
+hs -c "spaces.update($params)"
