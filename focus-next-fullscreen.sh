9a25723188eefdf066e25dab9a60627e59575588
Jens Fredskov <jensfredskov@protonmail.ch>
2020-03-24 11:34:04 +0100

    YOLO: It's Working!

--- focus-next-fullscreen.sh
+++ focus-next-fullscreen.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env sh
 
 active_space=$(yabai -m query --spaces --space | jq -r '.index')
-next_space=$(yabai -m query --spaces | jq -r 'limit(1;.[] | select(."native-fullscreen" == 1)) | .index')
+last_fullscreen_space=$(
+  yabai -m query --spaces |
+    jq -r '[.[] | select(."native-fullscreen" == 1)] | last | .index'
+)
 
-yabai -m space --focus "$next_space"
+if [ "$active_space" -ge "$last_fullscreen_space" ]; then
+  first_fullscreen_space=$(
+    yabai -m query --spaces |
+      jq -r '[.[] | select(."native-fullscreen" == 1)] | first | .index'
+  )
+  yabai -m space --focus "$first_fullscreen_space"
+else
+  next_fullscreen_space=$(
+    yabai -m query --spaces |
+      jq -r "[.[] | select(.\"native-fullscreen\" == 1 and .index > $active_space)] | first | .index"
+  )
+  yabai -m space --focus "$next_fullscreen_space"
+fi
